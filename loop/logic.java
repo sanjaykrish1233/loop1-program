a7a8a7d215d25a84fe6b0d13157ef127a483c5de
sanjaykrish1233 <sanjaykrishanaraj@gmail.com>
2026-06-03 10:08:45 +0530

    Add files via upload

---
+++ loop/logic.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class assign{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter number:");
+int a = sc.nextInt();
+a+=5;
+System.out.println("add:"+a);
+a-=5
+System.out.println("sub:"+a);
+a*=5;
+System.out.println("multiply:"+a);
+a/=5;
+System.out.println("Division:'+a);
+a%=5;
+System.out.println("Modulud:"+a);
+}
+}
